9d1572ba1bdde2dc5d9de25ebcc36503e38f0186
GabSichiroli <Sichiroli07@gmail.com>
2025-05-14 11:11:38 -0300

    commit

--- Gabi/3.1.py
+++ Gabi/3.1.py
@@ -1,7 +1,6 @@
 #Bibliotecas:
 
 import os
-import sys
 
 #Tela Inicial (Ainda não finalizada)
 def telainicial ():
@@ -21,24 +20,35 @@ def escolha():
     x=int(input("Insira a opção:"))
     os.system('cls')
     return x
+def cadastro_voos(voos):
+    print("Cadastro")
+    input("\t\t\n\t PRESIONE ENTRER PARA ENTRAR")
+    os.system('cls')
 #Main
+dicvoo={}
 telainicial()
 print("ok")
-op=escolha()
-print(f"{op}")
-match op:
-    case 1:
-        print("ok1")
-    case 2:
-        print("ok2")
-    case 3:
-        print("ok")
-    case 4:
-        print("ok4")
-    case 5:
-        print("ok5")
-    case 6:
-        print("ok6")
-    case 7:
-        print("ok7")
-        sys.exit(0)
+while True:
+    op=escolha()
+    print(f"{op}")
+    match op:
+        case 1:
+            print("ok1")
+            os.system('cls')
+            cadastro_voos(dicvoo)
+        case 2:
+            print("ok2")
+        case 3:
+            print("ok")
+        case 4:
+            print("ok4")
+        case 5:
+            print("ok5")
+        case 6:
+            print("ok6")
+        case 7:
+            print("ok7")
+            break
+        case _:
+            print("Opção Invalida")
+            escolha()
